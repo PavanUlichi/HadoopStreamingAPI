1f3e9b186bdf753e312be3a4a781f0e82b6260a1
ulichitr <pavan.ulichi@gmail.com>
2016-12-08 20:09:28 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -2,12 +2,16 @@
 
 import sys
 
+#for skipping data analysis on the first line
 for j, line in enumerate(sys.stdin, -1):
         if (j>=0):
+                #taking each line into a variable "line"
                 line = line.strip()
+                #taking all the words in the line separated by comma to variable line
                 words = line.split(",")
                 count = 0
                 length = len(words)
+                #we are considering the last 5 elements of the array
                 for i in reversed(words):
                         if ( count < 5 and i != ""):
                                 print '%s\t%s' % (i, 1)
